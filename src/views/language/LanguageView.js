b37277c06590b0870c64333a6b1555f9474994cb
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-20 17:56:17 +0200

    cleaned language view skeleton

--- src/views/language/LanguageView.js
+++ src/views/language/LanguageView.js
@@ -1,14 +1,10 @@
 import React, {Component} from 'react';
-import {Header} from 'layout/header/Header';
-import {AbstractSegment} from 'common/AbstractSegment';
 
 export class LanguageView extends Component {
 
   render = () => (
       <div className={'language-view'}>
         <h1>Language View</h1>
-        <Header/>
-        <AbstractSegment/>
       </div>
   );
 }
