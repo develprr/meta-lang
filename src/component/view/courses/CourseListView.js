e4efc6615c225a61ff5643a3829515fbda207dda
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-29 11:13:48 +0200

    configured MetaGrid in Courses list view

--- src/component/view/courses/CourseListView.js
+++ src/component/view/courses/CourseListView.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connectToState} from 'metamatic';
 import {STORE_COURSE} from '../../../store/course/CourseStore';
+import {MetaGrid} from '../../common/MetaGrid';
 
 export class CourseListView extends Component {
 
@@ -12,12 +13,46 @@ export class CourseListView extends Component {
   componentDidMount = () => connectToState(
       this,
       STORE_COURSE,
-      'list', (list) => this.setState({...this.state, list})
+      'list', (courses) => this.setState({...this.state, courses})
   );
+
+  getGridColumns = () => [
+    {
+      id: '_id',
+      index: 0,
+      visible: false
+    },
+    {
+      id: 'name',
+      index: 1,
+      sortIndex: 0,
+      sortDirection: 'asc',
+      title: 'Course Name',
+      visible: true
+    },
+    {
+      id: 'description',
+      index: 2,
+      sortIndex: 0,
+      sortDirection: 'asc',
+      title: 'Course Description',
+      visible: true
+    }
+  ];
+
+  onColumnChange = () => {
+  }
+
+  getCourses = () => this.state.courses || [];
 
   render = () => (
       <div className={'course-list-view'}>
-        <h1>Courses View</h1>
+        <h1>Available Courses</h1>
+        <MetaGrid columns={this.getGridColumns()}
+                  onColumnChange={this.onColumnChange}
+                  data={this.getCourses()}
+                  pageIndex={0}
+                  pageSize={100}/>
       </div>
   );
 }
