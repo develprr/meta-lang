2d7b9a9c762e304a74f62ab87e381c340d6d6060
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-29 02:05:28 +0200

    implemented a grid component

---
+++ src/component/common/MetaGrid.js
@@ -0,0 +1,116 @@
+import React, {Component} from 'react';
+import sortBy from 'lodash';
+
+const classNames = require('classnames');
+
+export class MetaGrid extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPageIndex: props.currentPageIndex || 0,
+      pageSize: props.pageSize || 10
+    };
+  }
+
+  getColumns = () => this.props.columns || [];
+
+  getVisibleColumns = () => this.getColumns().filter(column => column.visible);
+
+  getSortIconName = (column) => ({
+    'asc': 'arrow_drop_down',
+    'desc': 'arrow_drop_up'
+  }[column.sortDirection]);
+
+  changeSortOrder = (changeColumn) => {
+    const sortDirection = changeColumn.sortDirection || 'asc';
+    const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+    const columns = this.getColumns().map(column => column.id === changeColumn.id ? ({
+      ...column,
+      sortDirection: newSortDirection,
+      sortIndex: 0
+    }) : ({
+      ...column,
+      sortIndex: column.sortIndex + 1
+    }));
+    this.props.onColumnChange(columns);
+  };
+
+  renderSortIcon = (column) => column.sortDirection && (
+    <i className={'material-icons column-sort-icon'}
+       onClick={() => this.changeSortOrder(column)}
+    >
+      {this.getSortIconName(column)}
+    </i>
+  );
+
+  renderHeaderColumn = (column) => (
+    <th>
+      <span className={'header-column-title'}>
+        {column.title}
+      </span>
+      {this.renderSortIcon(column)}
+    </th>
+  );
+
+  getColumnsSortedBySortIndex = () => sortBy(this.getVisibleColumns(), 'sortIndex', 'asc');
+
+  getData = () => this.props.data || [];
+
+  sortData = (data) => {
+    const columns = this.getColumnsSortedBySortIndex();
+    return this.sortAlphabetically(data, columns[0]);
+  };
+
+  sortAlphabetically = (array, column) => array.sort((a, b) => {
+    const sortDirectionSwapper = column.sortDirection === 'desc' ? -1 : 1;
+    const nameA = a[column.id].toLowerCase();
+    const nameB = b[column.id].toLowerCase();
+    if (nameA < nameB) {
+      return -1 * sortDirectionSwapper;
+    }
+    if (nameA > nameB) {
+      return 1 * sortDirectionSwapper;
+    }
+    return 0;
+  });
+
+  getSortedData = () => this.sortData(this.getData());
+
+  renderCell = (dataItem, colId) => (
+    <td className={'data-cell'}>
+      {dataItem[colId]}
+    </td>
+  );
+
+  renderDataCell = (column, dataCell) => {
+    const renderFunction = column.renderCell || this.renderCell;
+    return renderFunction(dataCell, column.id);
+  };
+
+  getRowClass = (dataItem) => dataItem.active && 'active';
+
+  renderDataItemAsRow = (dataItem, index) => (
+    <tr key={index} className={classNames('data-row', this.getRowClass(dataItem))}>
+      {this.getVisibleColumns().map(column => this.renderDataCell(column, dataItem))}
+    </tr>
+  );
+
+  renderDataRows = () => this.getSortedData().map(this.renderDataItemAsRow);
+
+  renderHeaderRow = () => (
+    <tr className={'header-row'}>
+      {this.getVisibleColumns().map(this.renderHeaderColumn)}
+    </tr>
+  );
+
+  render = () => (
+    <table className={'meta-grid'}>
+      <tbody>
+      {this.renderHeaderRow()}
+      {this.renderDataRows()}
+      </tbody>
+    </table>
+  );
+
+}
